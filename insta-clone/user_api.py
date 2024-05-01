88574de592628d82d3afb0396f6bfc7e0c5de05a
SunHee <76764643+sunni546@users.noreply.github.com>
2024-05-01 18:07:29 +0900

    feat: 팔로우(Following) API에서 언팔로우 기능 추가
    사용자가 팔로우(언팔로우) 시 사용자들의 팔로잉/팔로워 수 +1(-1)

--- insta-clone/user_api.py
+++ insta-clone/user_api.py
@@ -42,8 +42,7 @@ class Join(Resource):
         elif get_type(phone_email) == 'email':
             user = User(email=phone_email, name=name, nickname=nickname, password=password_hash)
         else:
-            result = "회원가입 실패 - 잘못된 형식의 전화번호 or 이메일"
-            return jsonify({'result': result})
+            return jsonify({'result': "회원가입 실패 - 잘못된 형식의 전화번호 or 이메일입니다."})
 
         try:
             db.session.add(user)
@@ -122,6 +121,7 @@ class Following(Resource):
     def post(self, id):
         """
           Create a new follow item.
+          Update a user item.
         """
         """
           Request:
@@ -136,25 +136,81 @@ class Following(Resource):
         token = request.headers.get('Authorization')
 
         if not validate_token(token):
-            return "로그인 실패"
+            return jsonify({'result': "로그인 실패"})
 
         user_id = get_user_id(token)
         print(user_id, id)
 
-        follow = Follow(following_id=id, user_id=user_id)
+        if user_id == id:
+            return jsonify({'result': "팔로우 실패 - 동일한 사용자입니다."})
 
         try:
-            db.session.add(follow)
-            db.session.commit()
+            follow = Follow.query.filter_by(user_id=user_id, following_id=id).first()
+            if follow:
+                result = {'result': "팔로우 실패 - 이미 팔로우 중인 사용자입니다."}
 
-            result = {
-                "id": follow.id,
-                "following_id": follow.following_id,
-                "user_id": follow.user_id
-            }
+            else:
+                follow = Follow(following_id=id, user_id=user_id)
+                db.session.add(follow)
+
+                user = db.session.get(User, user_id)
+                user.following_number += 1
+
+                following = db.session.get(User, id)
+                following.follower_number += 1
+
+                db.session.commit()
+
+                result = {
+                    "id": follow.id,
+                    "following_id": follow.following_id,
+                    "user_id": follow.user_id
+                }
 
         except Exception as e:
             print(e)
+            result = {'result': "팔로우 실패"}
+
+        return jsonify(result)
+
+    def delete(self, id):
+        """
+          Delete a follow item.
+        """
+        """
+          Request:
+            DELETE /users/follow/2
+          Returns:
+            {}
+        """
+        token = request.headers.get('Authorization')
+
+        if not validate_token(token):
+            return jsonify({'result': "로그인 실패"})
+
+        user_id = get_user_id(token)
+        print(user_id, id)
+
+        if user_id == id:
+            return jsonify({'result': "언팔로우 실패 - 동일한 사용자입니다."})
+
+        try:
+            follow = Follow.query.filter_by(user_id=user_id, following_id=id).first()
+
+            db.session.delete(follow)
+
+            user = db.session.get(User, user_id)
+            user.following_number -= 1
+
+            following = db.session.get(User, id)
+            following.follower_number -= 1
+
+            db.session.commit()
+
             result = {}
+
+        except Exception as e:
+            print(e)
+            result = {'result': "언팔로우 실패"}
 
         return jsonify(result)

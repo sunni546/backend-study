9092de88df942a01366704f4db24979da889d121
SunHee <76764643+sunni546@users.noreply.github.com>
2024-05-03 21:48:27 +0900

    feat: 게시물 수정 & 삭제 API

--- insta-clone/api/post_api.py
+++ insta-clone/api/post_api.py
@@ -107,6 +107,96 @@ class PostCR(Resource):
         return jsonify(result)
 
 
+@Post_api.route('/<int:id>')
+@Post_api.doc(params={'id': 'Post ID'})
+class PostUD(Resource):
+    def patch(self, id):
+        """
+          Update a post item.
+        """
+        """
+          Request:
+            PATCH /posts/1
+            Request:
+            {
+              "content": "content1-1"
+            }
+          Returns:
+            {
+              "id": 1,
+              "content": "content1-1",
+              "image": "image1",
+              "created_at": 2024-05-02 14:12:13,
+              "like_number": 0,
+              "comment_number": 0
+            }
+        """
+        token = request.headers.get('Authorization')
+
+        if not validate_token(token):
+            return jsonify({'result': "로그인 실패"})
+
+        user_id = get_user_id(token)
+        content = request.json.get('content')
+        print(user_id, id, content)
+
+        try:
+            post = db.session.get(Post, id)
+
+            if post.user_id != user_id:
+                return jsonify({'result': "수정 실패 - 권한 없음"})
+
+            post.content = content
+            db.session.commit()
+
+            result = make_result(post)
+
+        except Exception as e:
+            print(e)
+            result = {'result': "수정 실패"}
+
+        return jsonify(result)
+
+    def delete(self, id):
+        """
+          Delete a post item.
+        """
+        """
+          Request:
+            DELETE /posts/1
+          Returns:
+            {}
+        """
+        token = request.headers.get('Authorization')
+
+        if not validate_token(token):
+            return jsonify({'result': "로그인 실패"})
+
+        user_id = get_user_id(token)
+        print(user_id, id)
+
+        try:
+            post = db.session.get(Post, id)
+
+            if post.user_id != user_id:
+                return jsonify({'result': "삭제 실패 - 권한 없음"})
+
+            db.session.delete(post)
+
+            user = db.session.get(User, user_id)
+            user.post_number -= 1
+
+            db.session.commit()
+
+            result = {}
+
+        except Exception as e:
+            print(e)
+            result = {'result': "삭제 실패"}
+
+        return jsonify(result)
+
+
 def make_result(post):
     result = {
         "id": post.id,
